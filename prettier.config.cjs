a209161deb3ef8a151b7a85648dd2996bfa04bf9
Aaron Durant <aaron@aarondurant.com>
2025-12-11 15:34:52 -0500

    Add project tooling and quality gate

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** Lucid prettier config – keep this in sync across projects if it feels right */
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 80,
+  tabWidth: 2,
+  arrowParens: "always",
+};
